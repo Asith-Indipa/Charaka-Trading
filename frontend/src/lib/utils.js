a1c9ac88e53f39e4f218b45d6cf922e1abceb9a0
asith <asithindipa07@gmail.com>
2026-04-01 15:12:14 +0530

    Update

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'LKR',
+    maximumFractionDigits: 0,
+  }).format(value);
+}
+
+export function formatCompactNumber(value) {
+  return new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    compactDisplay: 'short',
+    maximumFractionDigits: 1,
+  }).format(value);
+}
+
+export function formatCompactCurrency(value) {
+  return 'LKR ' + formatCompactNumber(value);
+}
